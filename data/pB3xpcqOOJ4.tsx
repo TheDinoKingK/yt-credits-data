7b903d61fb51612be2e440f5844c77a20e068c99
TheDinoKingK <kaveencode365@gmail.com>
2026-01-11 12:47:38 -0500

    rough tsx code to test tsx remote rendering

---
+++ data/pB3xpcqOOJ4.tsx
@@ -0,0 +1,49 @@
+<div>
+  <TitleCredit>The Vigilante! (No Hit)</TitleCredit>
+  <h2>featured:</h2>
+    <p>Sammy Mayo</p>
+    <p>Good Question</p>
+  <h2>assets:</h2>
+    <p>Dino's Portrait Drawing (made by riri)</p>
+    <p></p>
+</div>
+
+// title: The Vigilante! (No Hit)
+// featured:
+//   - name: Sammy Mayo
+//     note: The girl who asked if i'm fake
+//   - name: Good Question
+//     note: 'The guy in the discord screenshot, asking if i''m ai generated'
+//     links:
+//       - platform: twitch
+//         url: 'https://www.twitch.tv/thats_a_goodquestion/'
+// assets:
+//   - title: dino's portrait drawing
+//     desc: >-
+//       used for the little horror bit, removed the pupils, eyebrows, & smile in
+//       photoshop
+//     creators:
+//       - name: Riri
+//         links:
+//           - platform: fiverr
+//             url: 'https://www.fiverr.com/mizuholily'
+//   - title: vibe check hand
+//     desc: used in the horror bit as the hand my character is using to grab you
+//     source:
+//       note: genuinely can't remember where I first got this from
+//   - title: ai generated raptor
+//     desc: used when I said 'why would i be ai generated?' as an example
+//     source:
+//       note: >-
+//         I'm gonna be honest I just searched up 'ai generated raptor' and that
+//         image popped up.
+//   - title: iconic green-screen explosion
+//     desc: >-
+//       the iconic (and not great quality) green-screen explosion from youtube!
+//       (used at the end of the video)
+//     source:
+//       url: 'https://www.youtube.com/watch?v=TQUut8S9SRc'
+// notes:
+//   - note: >-
+//       idk why but i beat the bosses out of order, the first boss, then third,
+//       and now second.
